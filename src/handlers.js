988a253b7985ea866cc6b48daffa978b9e295a7c
Pedro Luis Dias <luisp.diias@gmail.com>
2025-12-10 00:15:26 -0300

    feat: implement index handler and add HTML template for index page

--- src/handlers.js
+++ src/handlers.js
@@ -1,4 +1,16 @@
-export const indexhandler = (req, res) => {
-  res.writeHead(200, { "content-type": "text/plain" });
-  res.end("Ok");
+import { getHtmlContent } from "./utils.js";
+
+export const notFound = (req, res) => {
+  res.writeHead(404);
+  res.end("not found.");
+};
+
+export const indexhandler = async (req, res) => {
+  const template = await getHtmlContent("index.html");
+  if (!template) {
+    notFound(req, res);
+    return;
+  }
+  res.writeHead(200, { "content-type": "text/html" });
+  res.end(template, "utf-8");
 };
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,23 @@
+import fs from "fs/promises";
+
+const htmlViewsPath = "./public/views/";
+
+export const getHtmlContent = async (fName) => {
+  try {
+    const content = await fs.readFile(htmlViewsPath + fName, {
+      encoding: "utf8",
+    });
+
+    return content;
+  } catch (err) {
+    log(
+      `error on read html file ${htmlViewsPath + fName}: ${err.message}`,
+      "error"
+    );
+    return undefined;
+  }
+};
+
 export const log = (message, logType) => {
   const logColors = {
     error: "\x1b[31m",
